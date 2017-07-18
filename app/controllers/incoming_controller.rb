837c0e61b3f1eae726a792211a2277a0321ba222
SWLivingstone <scottwlivingstone@gmail.com>
2017-07-18 14:56:46 -0700

    Heroku TEST
    Add IncomingMail#create

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,4 +1,25 @@
 class IncomingController < ApplicationController
+
   def create
+   puts "INCOMING PARAMS HERE: #{params}"
+
+   @user = User.find_by(email: params[:sender])
+   @topic = Topic.find_by(title: params[:subject])
+
+   if @user == nil
+     @user = User.create(email: params[:sender], password: RandomData.random_word)
+   end
+
+   if @topic == nil
+     @topic = Topic.create(title: params[:subject])
+   end
+
+    @bookmarks_array = params["stripped-text"].split(/,/)
+
+    @bookmarks_array.each do |bookmark|
+      Bookmark.create(url: bookmark, topic: @topic)
+    end
+
+   head 200
   end
 end
